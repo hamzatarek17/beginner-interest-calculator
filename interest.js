4866debd37bf14c8dde6ab30be7b780c43c90b4b
hamzatarek17 <166939951+hamzatarek17@users.noreply.github.com>
2024-06-17 18:18:57 +0300

    Add files via upload

---
+++ interest.js
@@ -0,0 +1,29 @@
+function calculator(){
+    const totalAmount = document.getElementById("total-amount");
+    const principalInput = document.getElementById("principal");
+    const rateInput = document.getElementById("rate");
+    const yearsInput = document.getElementById("years");
+
+    let principal = Number(principalInput.value);
+    let rate = Number(rateInput.value / 100);
+    let years = Number(yearsInput.value);
+
+    if(principal < 0 || isNaN(principal)){
+        principal = 0
+        principalInput = 0
+    }
+
+    if(rate < 0 || isNaN(rate)){
+        rate = 0
+        rateInput = 0
+    }
+
+    if(years < 0 || isNaN(years)){
+        years = 0
+        yearsInput = 0
+    }
+
+    const result = principal * Math.pow((1 + rate / 1), 1*years)
+    
+    totalAmount.textContent  = result.toLocaleString(undefined, {style: "currency", currency:"USD"})
+}
